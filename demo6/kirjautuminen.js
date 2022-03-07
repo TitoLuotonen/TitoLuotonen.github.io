42da0ca286f04eecc05a9d816fe9c66fb000aa44
TitoLuotonen <99165876+TitoLuotonen@users.noreply.github.com>
2022-03-07 09:26:06 +0200

    ghfgryhgt

--- demo6/kirjautuminen.js
+++ demo6/kirjautuminen.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 
 function onkoKirjautunut(){
-    let kirjautunut = 'kyllä';
-    if(kirjautunut === 'kyllä'){
+    let kirjautunut = 'kylla';
+    if(kirjautunut === 'kylla'){
         document.getElementById('tervetulo_teksti').textContent += ' Tito!';
     }
 }
